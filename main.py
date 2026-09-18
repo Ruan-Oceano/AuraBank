0e40d9a81af00290c611538931ccfb03d9f7ed57
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-09-18 19:29:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ while True:
         depositar()
     elif opcao == "5":
         sacar()
-    elif opcao == 6:
+    elif opcao == "6":
         listar_contas()
     elif opcao == "7":
         print("Saindo...")
